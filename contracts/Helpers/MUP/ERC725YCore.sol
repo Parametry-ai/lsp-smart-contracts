f0432139260413a0e5542a2e00caa89f6d410104
YamenMerhi <yamennmerhi@gmail.com>
2022-02-08 16:39:05 +0200

    feat: add modified contracts

---
+++ contracts/Helpers/MUP/ERC725YCore.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+// constants
+import "./constants.sol";
+
+// interfaces
+import "./IERC725Y.sol";
+
+// modules
+import "@openzeppelin/contracts/utils/introspection/ERC165Storage.sol";
+import "./MOwnableUnset.sol";
+
+/**
+ * @title Core implementation of ERC725 Y General key/value store
+ * @author Fabian Vogelsteller <fabian@lukso.network>
+ * @dev Contract module which provides the ability to set arbitrary key value sets that can be changed over time
+ * It is intended to standardise certain keys value pairs to allow automated retrievals and interactions
+ * from interfaces and other smart contracts
+ */
+abstract contract ERC725YCore is MOwnableUnset, ERC165Storage, IERC725Y {
+    /**
+     * @dev Map the keys to their values
+     */
+    mapping(bytes32 => bytes) internal store;
+
+    /* Public functions */
+
+    /**
+     * @inheritdoc IERC725Y
+     */
+    function getData(bytes32[] memory keys)
+        public
+        view
+        virtual
+        override
+        returns (bytes[] memory values)
+    {
+        values = new bytes[](keys.length);
+
+        for (uint256 i = 0; i < keys.length; i++) {
+            values[i] = _getData(keys[i]);
+        }
+
+        return values;
+    }
+
+    /**
+     * @inheritdoc IERC725Y
+     */
+    function setData(bytes32[] memory _keys, bytes[] memory _values)
+        public
+        virtual
+        override
+        onlyOwner
+    {
+        require(
+            _keys.length == _values.length,
+            "Keys length not equal to values length"
+        );
+        for (uint256 i = 0; i < _keys.length; i++) {
+            _setData(_keys[i], _values[i]);
+        }
+    }
+
+    /* Internal functions */
+
+    /**
+     * @notice Gets singular data at a given `key`
+     * @param key The key which value to retrieve
+     * @return value The data stored at the key
+     */
+    function _getData(bytes32 key)
+        internal
+        view
+        virtual
+        returns (bytes memory value)
+    {
+        return store[key];
+    }
+
+    /**
+     * @notice Sets singular data at a given `key`
+     * @param key The key which value to retrieve
+     * @param value The value to set
+     */
+    function _setData(bytes32 key, bytes memory value) internal virtual {
+        store[key] = value;
+        emit DataChanged(key, value);
+    }
+}
